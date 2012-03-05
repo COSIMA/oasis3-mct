8ed238fa37a864274e01faae241fa257163ca52a
Laure Coquart <coquart@cerfacs.fr>
2012-03-05 09:35:48 +0000

    Rename routines oasis*_proto in oasis* .
    svn path=/trunk/oasis3-mct/; revision=36

--- lib/psmile/src/mod_oasis.F90
+++ lib/psmile/src/mod_oasis.F90
@@ -12,17 +12,17 @@ module mod_oasis
 
    use mod_prism_parameters
 
-   use mod_prism_method ,only: oasis_init_comp_proto     => prism_method_init
-   use mod_prism_method ,only: oasis_terminate_proto     => prism_method_terminate
-   use mod_prism_method ,only: oasis_get_localcomm_proto => prism_method_getlocalcomm
-   use mod_prism_method ,only: oasis_enddef_proto        => prism_method_enddef
+   use mod_prism_method ,only: oasis_init_comp     => prism_method_init
+   use mod_prism_method ,only: oasis_terminate     => prism_method_terminate
+   use mod_prism_method ,only: oasis_get_localcomm => prism_method_getlocalcomm
+   use mod_prism_method ,only: oasis_enddef        => prism_method_enddef
 
-   use mod_prism_part   ,only: oasis_def_partition_proto => prism_part_def
+   use mod_prism_part   ,only: oasis_def_partition => prism_part_def
 
-   use mod_prism_var    ,only: oasis_def_var_proto       => prism_var_def
+   use mod_prism_var    ,only: oasis_def_var       => prism_var_def
 
-   use mod_prism_getput_interface ,only: oasis_get_proto => prism_get_proto
-   use mod_prism_getput_interface ,only: oasis_put_proto => prism_put_proto
+   use mod_prism_getput_interface ,only: oasis_get => prism_get_proto
+   use mod_prism_getput_interface ,only: oasis_put => prism_put_proto
 
    use mod_prism_grid   ,only: oasis_start_grids_writing => prism_grid_start_grids_writing
    use mod_prism_grid   ,only: oasis_write_grid          => prism_grid_write_grid
@@ -31,7 +31,7 @@ module mod_oasis
    use mod_prism_grid   ,only: oasis_write_area          => prism_grid_write_area
    use mod_prism_grid   ,only: oasis_terminate_grids_writing => prism_grid_terminate_grids_writing
 
-   use mod_prism_sys    ,only: oasis_abort_proto         => prism_sys_abort
+   use mod_prism_sys    ,only: oasis_abort       => prism_sys_abort
 
    implicit none
 
