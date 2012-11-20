5acafa6425e7ab887ab49d756fa1596a240a9fb0
Laure Coquart <coquart@cerfacs.fr>
2012-11-20 08:45:12 +0000

    Modify source code to run on corail when they are not coupled with oasis3-mct (comp_id was not defined).
    svn path=/trunk/oasis3-mct/; revision=371

--- examples/tutorial/model2.F90
+++ examples/tutorial/model2.F90
@@ -112,7 +112,6 @@ PROGRAM model2
   WRITE (w_unit,*) TRIM(comp_name), ' Running with reals compiled as kind =',wp
   WRITE (w_unit,*) 'I am component ', TRIM(comp_name), ' rank :',rank
   WRITE (w_unit,*) '----------------------------------------------------------'
-  WRITE(w_unit,*) 'I am the ', TRIM(comp_name), ' ',' comp', comp_id, ' local rank ', mype
   CALL flush(w_unit)
   !
   !!!!!!!!!!!!!!!!! OASIS_GET_LOCALCOMM !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -133,6 +132,7 @@ PROGRAM model2
       CALL oasis_abort(comp_id,comp_name,'Problem at line 133')
   ENDIF
   !
+  WRITE(w_unit,*) 'I am the ', TRIM(comp_name), ' local rank ', mype
   WRITE (w_unit,*) 'Number of processors :',npes
   CALL flush(w_unit)
   !
