1e9e3cc807e37949a8a0045690ecc1c9c68eb590
Laure Coquart <coquart@cerfacs.fr>
2013-10-31 09:53:27 +0000

    Add modifications for conservative interpolation (ticket 524): - calculate nearest neighbours for target points out of the source domain - weights put to 0 for masked target points
    svn path=/trunk/oasis3-mct/; revision=844

--- lib/scrip/src/fracnnei.f
+++ lib/scrip/src/fracnnei.f
@@ -1,7 +1,8 @@
       subroutine fracnnei (src_size, dst_size,
      $    ld_srcmask, ld_dstmask,
      $    src_lon, src_lat, dst_lon, dst_lat,
-     $    num_links, num_wgts,
+     $    num_links, num_wgts, num_neigh, lnnei,
+     $    weights_temp, src_addr_temp, dst_addr_temp,
      $    weights, src_addr, dst_addr)
 C****
 C               *****************************
@@ -82,21 +83,36 @@ C
      $    ld_dstmask(dst_size)    ! target grid mask
 C
       INTEGER (kind=int_kind) ::
-     $    num_links,      ! number of links between src and tgt
-     $    num_wgts        ! number of weights
+     $    num_links,       ! number of links between src and tgt
+     $    num_wgts,        ! number of weights
+     $    num_neigh        ! number of Vmm points
 
-C
-C* ---------------------------- Intent InOut ------------------------------
-C
+      logical (kind=log_kind) ::
+     $    lnnei(dst_size)       ! flag for tricky points
+
+
       REAL (kind=dbl_kind) ::
-     $    weights(num_wgts, num_links) ! remapping weights
+     $    weights_temp(num_wgts, num_links) ! oldsize remapping weights
 C
       INTEGER (kind=int_kind) ::
-     $    src_addr(num_links), ! remapping source addresses
-     $    dst_addr(num_links)  ! remapping target addresses
+     $    src_addr_temp(num_links), ! oldsize remapping source addresses
+     $    dst_addr_temp(num_links)  ! oldsize remapping target addresses
+
+     
+C
+C* ---------------------------- Intent Out ------------------------------
+C
+      REAL (kind=dbl_kind)  ::
+     $    weights(num_wgts, num_links+num_neigh ) ! remapping weights
+C
+      INTEGER (kind=int_kind)  ::
+     $    src_addr(num_links+num_neigh), ! remapping source addresses
+     $    dst_addr(num_links+num_neigh)  ! remapping target addresses
 C
 C* ---------------------------- Local declarations ----------------------
 C
+C
+
 C
       INTEGER (kind=int_kind) :: 
      $    ila_nneiadd           ! Nearest-neighbor address
@@ -104,19 +120,7 @@ C
       INTEGER (kind=int_kind) ::
      $    ib_dst,               ! INDEX loop for the distance grid
      $    ib_src,               ! INDEX loop for the source grid
-     $    ib_neigh,             ! INDEX loop on the corresponding src pts
-     $    ib_links              ! INDEX loop for the links      
-C
-      INTEGER (kind=int_kind) ::
-     $    nb_Vmm,               ! number of Mtt points
-     $    ntottotland,          ! number of land points
-     $    ntotland,             ! number of land points
-     $    ntotoce,              ! number of oceanic points
-     $    ntotngh               ! number of corresponding source points
-C
-      INTEGER (kind=int_kind) ::
-     $    beg_links,            ! begining of the serie of links
-     $    nb_links              ! number of links
+     $    ib_links              ! INDEX loop for the links
 C
       REAL (kind=dbl_kind) ::
      $    coslat,               ! cosinus of the latitude
@@ -128,6 +132,8 @@ C
      $    arg
 C
       INTEGER (kind=int_kind) :: n, il
+C
+      INTEGER (kind=int_kind) :: counter_Vmm
 C
 C* ---------------------------- Poema verses ----------------------------
 C
@@ -147,7 +153,7 @@ C
           WRITE (UNIT = nulou,FMT = *) 
      $        ' Treating the tricky points of the remapping'
           WRITE (UNIT = nulou,FMT = *) ' '
-          CALL OASIS_FLUSH_SCRIP(nulou)
+          CALL FLUSH(nulou)
       ENDIF
 C
 C *----------------------------------------------------------------------
@@ -157,63 +163,28 @@ C        -------------------  m m
 C     The target point is a non-masked Valid point while the source points 
 C         are all masked points. Use of the non-masked nearest neighbours.
 C
-      nb_Vmm = 0
-      ntottotland = 0
-      beg_links = 1
-
-C* -- Loop all other the target points
-      DO ib_dst = 1, dst_size
-C* -- If the point is a sea point
-        IF (ld_dstmask(ib_dst)) THEN
-
-            beg_links = 0
-
-            DO ib_links = 1, num_links
-              IF (dst_addr(ib_links) .eq. ib_dst) THEN
-                  beg_links = ib_links
-                  exit
-              ENDIF
-            END DO 
-
-            IF (beg_links .ne. 0) THEN
-
-                ntotland = 0
-                ntotoce = 0
-                ntotngh = 0
-
-C* -- Find the number of associated src points to the non-masked tgt point
-                nb_links = 1
-                DO il = beg_links+1, num_links
-                  IF (dst_addr(il) .eq. dst_addr(beg_links)) 
-     $                nb_links = nb_links + 1
-                END DO
-                
-C* -- For each point on the src grid associated to the non-masked tgt point
-                DO ib_neigh = 1, nb_links
-C
-                  ntotngh = ntotngh + 1
-
-C* -- Check IF the point is a masked or non-masked point and treat it
-                  IF (.not. ld_srcmask(src_addr(beg_links+ib_neigh-1)))
-     $                THEN
-                      ntotland = ntotland + 1
-                      ntottotland = ntottotland + 1
-                  ELSE IF (ld_srcmask(src_addr(beg_links+ib_neigh-1))) 
-     $                    THEN
-                      ntotoce = ntotoce + 1
-                  ELSE
-                      WRITE (nulou, *) 'Pb with ocean mask with Mtt 1' 
-                  END IF
-
-                END DO
  
-C* -- If all the src points are land, treat it !
-                IF (ntotland .EQ. ntotngh) THEN
-                    nb_Vmm = nb_Vmm + 1
-                    WRITE(nulou,*) 
-     $               '************ Doing FRACNNEI for point', ib_dst  
+C  -- store the weights, src_addr, dst_addr from temporary array
+      weights(1:num_wgts,1:num_links) =
+     $                  weights_temp(1:num_wgts,1:num_links)
+      src_addr(1:num_links) = src_addr_temp(1:num_links)
+      dst_addr(1:num_links) = dst_addr_temp(1:num_links)
 
-C* -- Find the nearest neighbours and change weights and address 
+C* -- Find the nearest neighbours and store weights and address 
+      counter_Vmm = 0
+      DO ib_dst = 1, dst_size
+        IF ( lnnei(ib_dst) .eqv. .true. ) THEN
+
+          counter_Vmm = counter_Vmm+1    
+          dst_addr(num_links+counter_Vmm) = ib_dst
+
+      IF (nlogprt .GE. 2) THEN
+          write(nulou,*) 'ib_dst for true=',ib_dst 
+          write(nulou,*) 'counter_Vmm =',counter_Vmm
+          write(nulou,*) 'num_links+counter_Vmm =',
+     $                   num_links+counter_Vmm 
+          write(nulou,*) 'dst_addr =',dst_addr(num_links+counter_Vmm)
+      ENDIF
 
                     coslat = cos(dst_lat(ib_dst))
                     sinlat = sin(dst_lat(ib_dst))
@@ -241,21 +212,18 @@ C* -- Find the nearest neighbours and change weights and address
                           ENDIF
                       ENDIF
                     END DO
-                    src_addr(beg_links) = ila_nneiadd
-                    weights(1,beg_links) = 1.0
-                    WRITE(nulou,*) 
+                    src_addr(num_links+counter_Vmm) = ila_nneiadd
+                    weights(1,num_links+counter_Vmm) = 1.0
+        IF (nlogprt .GE. 2) THEN
+            write(nulou,*) 'src_addr =',src_addr(num_links+counter_Vmm)
+            WRITE(nulou,*) 
      $               '*************** Nearest source neighbour is ', 
-     $                  ila_nneiadd                    
-                    IF (nb_links > 1) then
-                        DO n=2, nb_links
-                          src_addr(beg_links+n-1)=0
-                          weights(1,beg_links+n-1)=0.0
-                        END DO
-                    END IF
-                END IF
-            END IF
-        ENDIF
+     $                  ila_nneiadd          
+        ENDIF          
+      ENDIF
       END DO
+
+
 C
 C
 C *----------------------------------------------------------------------
@@ -265,7 +233,7 @@ C
           WRITE (UNIT = nulou,FMT = *) 
      $        '   Leaving ROUTINE fracnnei  -  Level 4'
           WRITE (UNIT = nulou,FMT = *) ' '
-          CALL OASIS_FLUSH_SCRIP(nulou)
+          CALL FLUSH(nulou)
       ENDIF
 
       END SUBROUTINE fracnnei
