a57089409f380c1ec69c15f6efadc734e1b06d46
Laure Coquart <coquart@cerfacs.fr>
2014-10-23 14:40:52 +0000

    Update: - put only low resolution grids - add remapping between curv to unstructured and unstructured to curv but CONSERV does not work yet : impossible to get the remapping files at the OASIS3-MCT format.
    svn path=/trunk/oasis3-mct/; revision=1096

--- examples/test_rmp_esmf/script_interp_with_esmf_curv_to_curv_grids.sh
+++ examples/test_rmp_esmf/script_interp_with_esmf_curv_to_curv_grids.sh
@@ -1,23 +1,26 @@
 #!/bin/ksh
 #
+HOMEDIR=`pwd`
+#
 # Name of the grids (4 characters)
 srcgrid=torc
 tgtgrid=lmdz
-# Remapping (done with SEMF in an NCL program see below)
+# Remapping (done with ESMF in an NCL program see below)
 interp_method=bilinear
-#interp_method=conserve
 #interp_method=patch
 #interp_method=neareststod
+#interp_method=conserve
 #
-HOMEDIR=/home/globc/coquart/oasis3-mct_perso_2.0/examples/test_rmp_esmf
 RUNDIR=$HOMEDIR/wkdir_esmf_${srcgrid}_${tgtgrid}_${interp_method}
 DATAGRIDS=$HOMEDIR/data_esmf
 [ -d $RUNDIR ] || mkdir $RUNDIR
 cd $RUNDIR
 #
+# Copy grids and masks files 
 ln -sf $DATAGRIDS/grids_all.nc $RUNDIR/grids.nc
 ln -sf $DATAGRIDS/masks_all.nc $RUNDIR/masks.nc
 #
 # Remapping using ESMF with NCL
-cp $HOMEDIR/interp_esmf_structured_one_step.ncl $RUNDIR/interp_esmf_structured_one_step.ncl
-ncl 'interp_method="'${interp_method}'"' 'srcgrid="'${srcgrid}'"' 'tgtgrid="'${tgtgrid}'"' interp_esmf_structured_one_step.ncl
+echo "Running interp_esmf_curv_to_curv_one_step.ncl"
+cp $HOMEDIR/interp_esmf_curv_to_curv_one_step.ncl $RUNDIR/interp_esmf_curv_to_curv_one_step.ncl
+ncl 'interp_method="'${interp_method}'"' 'srcgrid="'${srcgrid}'"' 'tgtgrid="'${tgtgrid}'"' interp_esmf_curv_to_curv_one_step.ncl
