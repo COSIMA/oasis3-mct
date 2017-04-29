43550ecce56bdbf305c40907eb2e07a713ad7099
Nicholas Hannah <nicjhannah@gmail.com>
2017-04-29 14:25:37 +1000

    Print stack trace when doing MCT abort.

--- lib/mct/mpeu/m_dropdead.F90
+++ lib/mct/mpeu/m_dropdead.F90
@@ -66,6 +66,12 @@ contains
 
   call MP_initialized(initialized,ier)
 
+#if defined(__INTEL_COMPILER)
+      call TRACEBACKQQ()
+#elif defined(__GFORTRAN__)
+      call BACKTRACE()
+#endif
+
   if (initialized) then
 
 	!-------------------------------------------------
@@ -78,6 +84,7 @@ contains
 
     write(stderr,'(z3.3,5a)') myrank,'.',myname_,	&
       ': from ',trim(where),'()'
+    flush(stderr)
 
 	! raise a condition to the OS
 
@@ -91,6 +98,7 @@ contains
 
     write(stderr,'(5a)') 'unknown rank .',myname_,	&
       ': from ',trim(where),'()'
+    flush(stderr)
 
 #ifdef ENABLE_UNIX_ABORT
     call abort
@@ -142,6 +150,12 @@ end subroutine die_
 
   logical :: initialized
 
+#if defined(__INTEL_COMPILER)
+      call TRACEBACKQQ()
+#elif defined(__GFORTRAN__)
+      call BACKTRACE()
+#endif
+
   write(lineno,'(i16)') line
 
   call MP_initialized(initialized,ier)
@@ -159,6 +173,7 @@ end subroutine die_
       ': from ',trim(where),'()',	&
       ', line ',trim(adjustl(lineno)),	&
       ' of file ',fnam
+      flush(stderr)
 
 	! raise a condition to the OS
 
@@ -175,6 +190,7 @@ end subroutine die_
       ': from ',trim(where),'()',	&
       ', line ',trim(adjustl(lineno)),	&
       ' of file ',fnam
+      flush(stderr)
 
 #ifdef ENABLE_UNIX_ABORT
     call abort
