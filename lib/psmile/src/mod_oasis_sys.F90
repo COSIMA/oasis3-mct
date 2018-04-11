f2b0ccc89b2130bb3abc1f88219322109fc7c58e
Nic Hannah <nic@doubleprecision.com.au>
2018-04-11 15:31:23 +1000

    Remove tracebackqq/backtrace because it causes segfault when there's a legitimate error. Closes #6

--- lib/psmile/src/mod_oasis_sys.F90
+++ lib/psmile/src/mod_oasis_sys.F90
@@ -39,7 +39,6 @@ CONTAINS
 !--------------------------------------------------------------------
 
 !> OASIS abort method, publically available to users
-
 SUBROUTINE oasis_abort(id_compid, cd_routine, cd_message, file, line, rcode)
 
    IMPLICIT NONE
@@ -77,12 +76,6 @@ SUBROUTINE oasis_abort(id_compid, cd_routine, cd_message, file, line, rcode)
    WRITE (nulprt,*) subname,astr,'on local  rank = ',mpi_rank_local
    WRITE (nulprt,*) subname,astr,'CALLING ABORT FROM OASIS LAYER NOW'
    CALL oasis_flush(nulprt)
-
-#if defined(__INTEL_COMPILER)
-    CALL TRACEBACKQQ()
-#elif defined(__GFORTRAN__)
-    CALL BACKTRACE()
-#endif
 
 #if defined use_comm_MPI1 || defined use_comm_MPI2
    CALL MPI_ABORT (mpi_comm_global, errcode, ierror)
